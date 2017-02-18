250be9f574486af7847d843480784a0216555d30
Grigoriy Kolodyazhniy <shakolo@gmail.com>
2017-02-18 23:26:16 +0200

    add Unit private var

---
+++ Unit.hpp
@@ -0,0 +1,15 @@
+#ifndef UNIT_HPP
+#define UNIT_HPP
+
+#include <iostream>
+
+class Unit {
+    private:
+        int state;
+        int ability;
+    public:
+        Unit();
+        ~Unit();
+};
+
+#endif //UNIT_HPP
